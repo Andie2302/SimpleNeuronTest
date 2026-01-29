1194116984ab02fb0d4d78dfedfeb4fe65f35dd1
andreas <Andreas.Zwander@gmail.com>
2026-01-29 12:00:51 +0100

    Add basic program to test `SimpleNetwork` functionality

---
+++ SimpleNeuronTest/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+var net = new SimpleNetwork();
+double[] sensorDaten = { 0.5, -0.2 }; // Beispiel-Inputs
+
+var result = net.Predict(sensorDaten);
+Console.WriteLine($"Netzwerk-Ausgang: {result:F4}");
