bcec710975aa74702924583a60766b4953dfbfca
andreas <Andreas.Zwander@gmail.com>
2026-01-29 12:10:24 +0100

    Add training logic and multi-input testing to `SimpleNetwork` example program

--- SimpleNeuronTest/Program.cs
+++ SimpleNeuronTest/Program.cs
@@ -1,9 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿using SimpleNeuronTest;
 
 var net = new SimpleNetwork();
-double[] sensorDaten = { 0.5, -0.2 }; // Beispiel-Inputs
 
-var result = net.Predict(sensorDaten);
-Console.WriteLine($"Netzwerk-Ausgang: {result:F4}");
+// Trainingsdaten für eine UND-Verknüpfung
+double[][] inputs = {
+    new double[] { 0, 0 },
+    new double[] { 0, 1 },
+    new double[] { 1, 0 },
+    new double[] { 1, 1 }
+};
+double[] targets = { 0, 0, 0, 1 };
+
+Console.WriteLine("Training startet...");
+
+for (int epoch = 0; epoch < 10000; epoch++)
+{
+    for (int i = 0; i < inputs.Length; i++)
+    {
+        net.Train(inputs[i], targets[i], 0.1);
+    }
+}
+
+Console.WriteLine("Training beendet.\nErgebnisse:");
+
+foreach (var input in inputs)
+{
+    double result = net.Predict(input);
+    Console.WriteLine($"Input: {input[0]}, {input[1]} -> Output: {result:F4}");
+}
