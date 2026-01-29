52ebde49321e75854084b521a3453a6cdf5cdd64
andreas <Andreas.Zwander@gmail.com>
2026-01-29 15:54:14 +0100

    Refactor `Layer` to use `IActivationFunction` for activation and derivative computations, enabling more flexible activation handling

--- SimpleNeuronTest/basic/Layer.cs
+++ SimpleNeuronTest/basic/Layer.cs
@@ -3,9 +3,11 @@ namespace SimpleNeuronTest.basic;
 public class Layer
 {
     public Neuron[] Neurons { get; }
+    private readonly IActivationFunction _activation;
 
-    public Layer(int neuronCount, int inputCountPerNeuron, Random rng)
+    public Layer(int neuronCount, int inputCountPerNeuron, IActivationFunction activation, Random rng)
     {
+        _activation = activation;
         Neurons = new Neuron[neuronCount];
         for (var i = 0; i < neuronCount; i++)
             Neurons[i] = new Neuron(inputCountPerNeuron, rng);
@@ -15,7 +17,13 @@ public class Layer
     {
         var outputs = new double[Neurons.Length];
         for (var i = 0; i < Neurons.Length; i++)
-            outputs[i] = Neurons[i].Forward(inputs);
+        {
+            double sum = Neurons[i].CalculateSum(inputs);
+            outputs[i] = _activation.Activate(sum);
+            Neurons[i].LastOutput = outputs[i];
+        }
         return outputs;
     }
+
+    public double GetDerivative(double output) => _activation.Derivative(output);
 }
