de56eb207ae26c0020c0a1b3ba17c4d83dc86cf7
andreas <Andreas.Zwander@gmail.com>
2026-01-29 14:25:50 +0100

    Refactor `LearnedXor` by introducing `NeuronWeights` struct and reusable methods for cleaner and more modular inference logic

--- SimpleNeuronTest/LearnedXor.cs
+++ SimpleNeuronTest/LearnedXor.cs
@@ -1,30 +1,28 @@
+
+
 namespace SimpleNeuronTest;
 
 public class LearnedXor
 {
-    // Die gelernten Konstanten aus deinem 10-Millionen-Lauf
-    private const double H0W0 = 7.48399429;
-    private const double H0W1 = -7.63352400;
-    private const double H0B  = -3.96533977;
+    private readonly record struct NeuronWeights(double W0, double W1, double Bias);
 
-    private const double H1W0 = 7.64419262;
-    private const double H1W1 = -7.43677222;
-    private const double H1B  = 3.71915677;
+    private static readonly NeuronWeights Hidden0 = new(7.48399429, -7.63352400, -3.96533977);
+    private static readonly NeuronWeights Hidden1 = new(7.64419262, -7.43677222, 3.71915677);
+    private static readonly NeuronWeights Output = new(14.77497287, -14.49729010, 7.03850020);
 
-    private const double OutW0 = 14.77497287;
-    private const double OutW1 = -14.49729010;
-    private const double OutB  = 7.03850020;
+    private static double Sigmoid01(double x) => 1.0 / (1.0 + Math.Exp(-x));
 
-    // Mathematische Hilfsfunktion
-    private static double Sigmoid(double x) => 1.0 / (1.0 + Math.Exp(-x));
-
-    public double Predict(double x0, double x1)
+    private static double Forward(in NeuronWeights neuron, double x0, double x1)
     {
-        // Hidden Layer Inferenz (ohne Schleifen, direkt berechnet)
-        double h0 = Sigmoid(x0 * H0W0 + x1 * H0W1 + H0B);
-        double h1 = Sigmoid(x0 * H1W0 + x1 * H1W1 + H1B);
+        var z = (x0 * neuron.W0) + (x1 * neuron.W1) + neuron.Bias;
+        return Sigmoid01(z);
+    }
 
-        // Output Layer Inferenz
-        return Sigmoid(h0 * OutW0 + h1 * OutW1 + OutB);
+    public static double Predict(double x0, double x1)
+    {
+        var h0 = Forward(Hidden0, x0, x1);
+        var h1 = Forward(Hidden1, x0, x1);
+
+        return Forward(Output, h0, h1);
     }
 }
