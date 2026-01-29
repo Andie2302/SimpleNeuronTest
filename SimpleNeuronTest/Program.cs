664d9d6995bc8a595a77e26f01e5630c0c39f649
andreas <Andreas.Zwander@gmail.com>
2026-01-29 12:35:14 +0100

    Use `var` in loop declarations and update array initialization syntax for consistency

--- SimpleNeuronTest/Program.cs
+++ SimpleNeuronTest/Program.cs
@@ -3,24 +3,25 @@
 var net = new SimpleNetwork();
 
 // Trainingsdaten für eine UND-Verknüpfung
-double[][] inputs = {
-    new double[] { 0, 0 },
-    new double[] { 0, 1 },
-    new double[] { 1, 0 },
-    new double[] { 1, 1 }
-};
+double[][] inputs =
+[
+    [0, 0],
+    [0, 1],
+    [1, 0],
+    [1, 1]
+];
 
 //logisches & ist das Ziel
 //double[] targets = { 0, 0, 0, 1 };
 
 //XOR ist das Ziel...
-double[] targets = { 0, 1, 1, 0 };
+double[] targets = [0, 1, 1, 0];
 
 Console.WriteLine("Training startet...");
 
-for (int epoch = 0; epoch < 10000; epoch++)
+for (var epoch = 0; epoch < 10000; epoch++)
 {
-    for (int i = 0; i < inputs.Length; i++)
+    for (var i = 0; i < inputs.Length; i++)
     {
         net.Train(inputs[i], targets[i], 0.1);
     }
