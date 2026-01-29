bc245de2240bc63df826bceedefded26ea7b18d1
andreas <Andreas.Zwander@gmail.com>
2026-01-29 14:49:13 +0100

    Call `RunXorTraining` in `Program.cs` to integrate XOR training logic

--- SimpleNeuronTest/basic/Program.cs
+++ SimpleNeuronTest/basic/Program.cs
@@ -1,6 +1,10 @@
 ﻿using SimpleNeuronTest.basic;
 
 
+
+TrainingTest.RunXorTraining();
+
+
 Console.WriteLine("Test der gelernten XOR-Klasse:");
 Console.WriteLine($"0, 0 -> {LearnedXor.Predict(0, 0):F4}");
 Console.WriteLine($"0, 1 -> {LearnedXor.Predict(0, 1):F4}");
